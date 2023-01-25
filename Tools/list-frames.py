7719c867d013a1d24667585eed54365e78494b14
Kacper Wołosik <63367300+mnb1188@users.noreply.github.com>
2023-01-25 16:38:24 +0100

    Removed unnecessary comments

--- Tools/list-frames.py
+++ Tools/list-frames.py
@@ -6,13 +6,11 @@ flist = []
 
 for path, subdirs, files in os.walk(r'./frames'):
     for filename in files:
-        #a.write(str(num) + str(" ") + str(filename) + os.linesep)
-        #num += 1
         flist.append(str(filename[:-4]))
 
 flist.sort()
 
 with open("output.txt", "w") as a:
     for fname in flist:
-        a.write(str(fname) + os.linesep)                #str(num) + str(" ") + 
+        a.write(str(fname) + os.linesep)          
         num += 1
